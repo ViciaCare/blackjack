c0c5a2616ab4f04ea99053785fcb715d4bdc5d56
ViciaCare <pvk.sakha@yandex.ru>
2021-06-12 11:17:00 +0900

    created class Deck

--- black_jack/black_jack_game.py
+++ black_jack/black_jack_game.py
@@ -5,3 +5,29 @@ ranks = pass
 values = pass
 
 playing = True
+
+
+class Card:
+    def __init__(self):
+        pass
+
+    def __str__(self):
+        pass
+
+
+class Deck:
+    def __init__(self):
+        self.deck = []
+        for suit in suits:
+            for rank in ranks:
+                pass
+
+    def __str__(self):
+        pass
+
+    def shuffle(self):
+        random.shuffle(self.deck)
+
+    def deal(self):
+        pass
+
