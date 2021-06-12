d23e318a84628fefd303141f398a5a52d0de5af2
ViciaCare <pvk.sakha@yandex.ru>
2021-06-12 11:25:03 +0900

    created class Chips

--- black_jack/black_jack_game.py
+++ black_jack/black_jack_game.py
@@ -42,3 +42,15 @@ class Hand:
 
     def adjust_for_ace(self):
         pass
+
+class Chips:
+    def __init__(self):
+        self.total = 100
+        self.bet = 0
+
+    def win_bet(self):
+        pass
+
+    def lose_bet(self):
+        pass
+
