61ad95a1dab3fdf6342620a5028ea16d296103da
ViciaCare <pvk.sakha@yandex.ru>
2021-06-12 11:22:07 +0900

    created class Hand

--- black_jack/black_jack_game.py
+++ black_jack/black_jack_game.py
@@ -31,3 +31,14 @@ class Deck:
     def deal(self):
         pass
 
+class Hand:
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+        self.aces = 0
+
+    def add_card(self, card):
+        pass
+
+    def adjust_for_ace(self):
+        pass
