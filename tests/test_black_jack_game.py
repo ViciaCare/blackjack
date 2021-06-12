ef1b5ae37246f8fe353b40bb825b726c7630e5f0
ViciaCare <pvk.sakha@yandex.ru>
2021-06-12 16:16:44 +0900

    edit class Hand and added tests

--- tests/test_black_jack_game.py
+++ tests/test_black_jack_game.py
@@ -41,20 +41,32 @@ class Deck:
         return single_card
 
 
+test_deck = Deck()
+print(test_deck)
+
 class Hand:
     def __init__(self):
         self.cards = []
         self.value = 0
         self.aces = 0
 
-
     def add_card(self, card):
         self.cards.append(card)
         self.value += values[card.rank]
 
-
     def adjust_for_ace(self):
         pass
+
+
+test_deck = Deck()
+test_deck.shuffle()
+test_player = Hand()
+test_player.add_card(test_deck.deal())
+test_player.add_card(test_deck.deal())
+test_player.value
+
+for card in test_player.cards:
+    print(card)
 
 
 class Chips:
