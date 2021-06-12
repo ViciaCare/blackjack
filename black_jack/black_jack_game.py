40c030d8512ecc84df6282beb184abe93da7883c
ViciaCare <pvk.sakha@yandex.ru>
2021-06-12 11:46:50 +0900

    created functions for handing game completion scenarios

--- black_jack/black_jack_game.py
+++ black_jack/black_jack_game.py
@@ -77,3 +77,23 @@ def show_some(player, dealer):
 def show_all(player, dealer):
     pass
 
+
+def player_busts():
+    pass
+
+
+def player_wins():
+    pass
+
+
+def dealer_busts():
+    pass
+
+
+def dealer_wins():
+    pass
+
+
+def push():
+    pass
+
