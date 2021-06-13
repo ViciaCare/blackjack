2e8e44a770297b37cb3d98c0d9cd714590eb3f3f
ViciaCare <pvk.sakha@yandex.ru>
2021-06-13 14:25:34 +0900

    the game movement

--- black_jack/black_jack_game.py
+++ black_jack/black_jack_game.py
@@ -165,5 +165,36 @@ while True:
     show_some(player_hand, dealer_hand)
 
     while playing:
-        #ask player, if he wants to draw an additional card or stay with the current cards
-        pass
+        hit_or_stand(deck, player_hand)
+
+        show_some(player_hand, dealer_hand)
+
+        if player_hand.value > 21:
+            player_busts(player_hand, dealer_hand, player_chips)
+            break
+
+    if player_hand.value <= 21:
+        while dealer_hand.value < 17:
+            hit(deck, dealer_hand)
+
+        show_all(player_hand, dealer_hand)
+
+        if dealer_hand.value > 21:
+            dealer_busts(player_hand, dealer_hand, player_chips)
+        elif dealer_hand.value > player_hand.value:
+            dealer_wins(player_hand, dealer_hand, player_chips)
+        elif dealer_hand.value < player_hand.value:
+            player_wins(player_hand, dealer_hand, player_chips)
+        else:
+            push(player_hand, dealer_hand)
+
+    print("\nThe amount of the player's chips - ", player_chips.total)
+
+    new_game = input("Do you want to play again? Enter 'y' or 'n' ")
+
+    if new_game[0].lower() == 'y':
+        playing = True
+        continue
+    else:
+        print('Thanks for playing!')
+        break
